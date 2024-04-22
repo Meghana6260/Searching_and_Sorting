3102563fa4a2988b465d7a76fa822833595887d9
Meghana6260 <167068736+Meghana6260@users.noreply.github.com>
2024-04-22 22:44:02 +0530

    Create Selection_sort.py

---
+++ Selection_sort.py
@@ -0,0 +1,30 @@
+def performSelectionSort(nums):
+    n = len(nums)
+  
+    for fixThisIndex in range(n - 1, 0, -1):
+        maxEle = nums[fixThisIndex]
+        maxEleIndex = fixThisIndex 
+ 
+        for index in range(fixThisIndex):
+            # 0 1 2 3 4 
+            if nums[index] > maxEle:
+                maxEleIndex = index 
+                maxEle = nums[index]
+        if fixThisIndex != maxEleIndex:
+            temp = nums[maxEleIndex]
+            nums[maxEleIndex] = nums[fixThisIndex]
+            nums[fixThisIndex] = temp
+ 
+        print(nums)
+nums = [12, 2, 34, 20, 56, 43, 45, 100, 89, 50]
+ 
+print("Before sorting:")
+print(nums)
+performSelectionSort(nums)
+ 
+print("After sorting:")
+print(nums)
+ 
+
+      
+      
